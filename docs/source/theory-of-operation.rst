2d23edf470fc78fdde6ce1a7e44d4229749ed853
v.stadnytskyi@gmail.com <v.stadnytskyi@gmail.com>
2019-10-07 12:59:11 -0400

    added documentation

--- docs/source/theory-of-operation.rst
+++ docs/source/theory-of-operation.rst
@@ -2,21 +2,58 @@
 Theory of Operation
 ===================
 
+------------
 Introduction
 ------------
+
 Buffers are widely used in the computer architecture to allow efficient data storage, handling and later retrieval.
 
 This project uses numpy array data structures to provide a fast and flexible data handling for streams of data. It is common to have a data acquisition unit that measures at a certain rate. The data from the internal buffer need to be read efficiently and should be available to other programs\devices at later time. The numpy array data structure in Python allows to preallocate memory at the initialization and the data handling from now on is extremely efficient.
 
-Design Specification
---------------------
 
-Benchmarks
-----------
-Configuration:
+Design Specification
+------------------------
 DI-4108 by DATAQ in 4kHz at 8 analog channels at 16bit resolution plus 1 digital channel will produce 68kB of data per second.
 
-TODO write a test to measure write/read time
-write time:
 
-read time:
+
+Benchmarks
+------------------------
+Configuration
+^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+:OS: macOS Mojave 10.14.6
+:Computer: MacBook Pro (13-inch, 2017, Two Thunderbolt 3 ports)
+:Processor: 2.3 GHz Intel Core i5
+:Memory: 16 GB 2133 MHz LPDDR3
+
+
+Write/Read time benchmarks
+^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+.. figure::  write-benchmarks-0.0.6.jpg
+   :align:   center
+
+
+.. figure::  read-benchmarks-0.0.6.jpg
+   :align:   center
+
+The numerical output :: python
+   >>> python3 examples/write-read\ benchmarks.py
+     circular buffer numpy version: 0.0.6
+     1.875741365999999e-06 per write and 1.3417917090000007e-06 per read of 1x10 array
+     1.4111644630000003e-05 per write and 1.3084426500000036e-06 per read of 10x10 array
+     0.00012056339729999994 per write and 1.2482045999998803e-06 per read of 100x10 array
+     0.001347408767000001 per write and 1.2678269999994995e-06 per read of 1000x10 array
+     0.01340139582999999 per write and 1.9024200000217206e-06 per read of 10000x10 array
+     2.005417302999998e-06 per write and 1.324801278999999e-06 per read of 1x100 array
+     1.650482488999998e-05 per write and 1.2904178099999797e-06 per read of 10x100 array
+     0.00013166361600000016 per write and 1.2773896999998869e-06 per read of 100x100 array
+     0.0012954091400000003 per write and 1.2730259999997885e-06 per read of 1000x100 array
+     0.013044410029999974 per write and 1.3796100000007528e-06 per read of 10000x100 array
+     1.2233516567000002e-05 per write and 1.455322498000001e-06 per read of 1x1000 array
+     0.00014691199192 per write and 1.2784705600000024e-06 per read of 10x1000 array
+     0.001376928726700001 per write and 1.3089801000006674e-06 per read of 100x1000 array
+     0.012413041053 per write and 1.5748430000002146e-06 per read of 1000x1000 array
+     0.12879166736000003 per write and 8.44166999996787e-06 per read of 10000x1000 array
--- examples/write-read benchmarks.py
+++ examples/write-read benchmarks.py
@@ -2,20 +2,21 @@
 # -*- coding: utf-8 -*-
 from circular_buffer_numpy import __version__
 from circular_buffer_numpy.circular_buffer import CircularBuffer
-print('circular buffer number version: {}'.format(__version__))
+print('circular buffer numpy version: {}'.format(__version__))
 import timeit
 data_dim = 1000
 buffer = CircularBuffer(shape=(1000000, data_dim))
-from numpy import random
+from numpy import random, asarray, where, savetxt
 data = random.randint(2**16, size=(1000, data_dim))
+from matplotlib import pyplot as plt
 
 def write():
     buffer.append(data)
 
-def read_N(N = 1000):
+def read():
     data = buffer.get_last_N(N)
 
-header = ['N','data_dim','number','time']
+header = ['N','data_dim','number','write time','read time']
 number_lst = [1000000,100000,10000,1000,100]
 N_lst = [1,10,100,1000,10000]
 data_dim_lst = [10,100,1000]
@@ -25,21 +26,33 @@ for data_dim in data_dim_lst:
     for N in N_lst:
         number = number_lst[N_lst.index(N)]
         data = random.randint(2**16, size=(N, data_dim))
-        t = timeit.timeit(write, number=number)
-        t_lst.append([N,data_dim,number,t/number])
-        print('{} per write of {}x{} array'.format(t/number,N,data_dim))
+        t_write = timeit.timeit(write, number=number)
+        t_read = timeit.timeit(read, number=number)
+        t_lst.append([N,data_dim,number,t_write/number,t_read/number])
+        print('{} per write and {} per read of {}x{} array'.format(t_write/number,t_read/number,N,data_dim))
+    del buffer
 
-del buffer
-def plot():
-    from matplotlib import pyplot as plt
-    fig = plt.figure()
-    from numpy import asarray, where
-    t_arr = asarray(t_lst)
-    plt.loglog(t_arr[where(t_arr[:,1] == 10),0],t_arr[where(t_arr[:,1] == 10),3],'-or', label = 'width = 10')
-    plt.loglog(t_arr[where(t_arr[:,1] == 100),0],t_arr[where(t_arr[:,1] == 100),3],'-og', label = 'width = 100')
-    plt.loglog(t_arr[where(t_arr[:,1] == 1000),0],t_arr[where(t_arr[:,1] == 1000),3],'-ob', label = 'width = 1000')
-    plt.xlabel('N, circular dimension length')
-    plt.ylabel('time to write')
-    plt.title('Time to write vs data dimensions (length x width)')
-    f.savefig("benchmarks-{}.pdf".format(__version__), bbox_inches='tight')
-plot()
+t_arr = asarray(t_lst)
+fig = plt.figure()
+
+
+plt.loglog(t_arr[where(t_arr[:,1] == 10),0],t_arr[where(t_arr[:,1] == 10),3],'-or')
+plt.loglog(t_arr[where(t_arr[:,1] == 100),0],t_arr[where(t_arr[:,1] == 100),3],'-og')
+plt.loglog(t_arr[where(t_arr[:,1] == 1000),0],t_arr[where(t_arr[:,1] == 1000),3],'-ob')
+plt.xlabel('N, circular dimension length')
+plt.ylabel('time to write')
+plt.title('Time to write vs data dimensions (length x width) \n where width equal to red: 10; green: 100; blue: 1000')
+fig.savefig("write-benchmarks-{}.jpg".format(__version__), bbox_inches='tight')
+
+fig = plt.figure()
+from matplotlib import pyplot as plt
+plt.loglog(t_arr[where(t_arr[:,1] == 10),0],t_arr[where(t_arr[:,1] == 10),4],'-or')
+plt.loglog(t_arr[where(t_arr[:,1] == 100),0],t_arr[where(t_arr[:,1] == 100),4],'-og')
+plt.loglog(t_arr[where(t_arr[:,1] == 1000),0],t_arr[where(t_arr[:,1] == 1000),4],'-ob')
+plt.xlabel('N, circular dimension length')
+plt.ylabel('time to read')
+plt.title('Time to write vs data dimensions (length x width) \n where width equal to red: 10; green: 100; blue: 1000')
+fig.savefig("read-benchmarks-{}.jpg".format(__version__), bbox_inches='tight')
+
+from numpy import savetxt
+savetxt("write-read-benchmarks-{}.txt".format(__version__),t_arr)
