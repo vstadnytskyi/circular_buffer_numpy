eda0bd4b1da564de453805d6489be47af2ae7108
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-10-03 23:37:49 +0000

    Update docs after building Travis build 54 of vstadnytskyi/circular_buffer_numpy
    The docs were built from the branch 'master' against the commit
    1dcb3b04ca995a4648b15a5f07dcda69cbdc693e.
    
    The Travis build that generated this commit is at
    https://travis-ci.com/vstadnytskyi/circular_buffer_numpy/jobs/394674826.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.8.post17.dev0+g07e6308',
+    VERSION: '0.0.9.post0.dev0+g1dcb3b0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
